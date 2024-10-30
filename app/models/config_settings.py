1aa57aae764da5aac10d669e85d9100b2344b4cb
SantiagoMag <santiagogem29@gmail.com>
2024-10-30 18:15:19 -0500

    UPD config_settings

--- app/models/config_settings.py
+++ app/models/config_settings.py
@@ -12,7 +12,7 @@ class ResConfigSettings(db.Model):
     """
     api_vm_url = db.Column(db.String(255), nullable=True)
 
-    def __init__(self, h2o_driverless_address, h2o_driverless_username, h2o_driverless_password, h2o_driverless_name_experiment, api_vm_url):
+    def __init__(self, api_vm_url):
         self.api_vm_url = api_vm_url
 
 
